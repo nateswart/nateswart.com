3fcdd8d139bd59eb855c41c76403bc32aceb4016
Nate Swart <nateswart@me.com>
2012-08-25 13:02:36 -0400

    Start of custom theme

---
+++ sites/all/themes/custom/nateswart/page--front.tpl.php
@@ -0,0 +1,12 @@
+<?php include_once('navbar.inc');?>
+
+<div class="container">
+
+<h1>Nate Swart</h1>
+<p class="lead">Senior Drupal Application Developer</p>
+<p>
+  <a href="http://twitter.com/nateswart" class="sb twitter">Twitter</a>
+  <a href="http://www.linkedin.com/in/nateswart" class="sb linkedin">LinkedIn</a>
+</p>
+
+</div> <!-- /container -->
---
+++ sites/all/themes/custom/nateswart/page.tpl.php
@@ -0,0 +1,10 @@
+<?php include_once('navbar.inc');?>
+
+<div class="container">
+  <h1><?php print $title; ?></h1>
+
+  <div class="hero-unit">
+  
+  </div>
+
+</div> <!-- /container -->
