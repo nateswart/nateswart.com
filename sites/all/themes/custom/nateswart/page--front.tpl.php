1d514739953e3f52bf72ff775132877f82c63e04
Nate Swart <nateswart@me.com>
2012-08-28 22:52:50 -0400

    Removed buttons from front page

--- sites/all/themes/custom/nateswart/page--front.tpl.php
+++ sites/all/themes/custom/nateswart/page--front.tpl.php
@@ -6,8 +6,6 @@
 <p class="lead">Senior Drupal Application Developer</p>
 <p>
   <?php print $messages; ?>
-  <a href="http://twitter.com/nateswart" class="sb text twitter">Twitter</a>
-  <a href="http://www.linkedin.com/in/nateswart" class="sb text linkedin">LinkedIn</a>
 </p>
 
 </div> <!-- /container -->
