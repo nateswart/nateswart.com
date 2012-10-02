7514dec0f61893d15e5b1fe55d3c502371fbffe3
Nate Swart <nateswart@me.com>
2012-10-02 19:22:28 -0400

    Switched to Piwik module

--- sites/all/themes/custom/nateswart/html.tpl.php
+++ sites/all/themes/custom/nateswart/html.tpl.php
@@ -30,18 +30,6 @@
     print $scripts;
   ?>
   
-  <!-- Piwik --> 
-  <script type="text/javascript">
-  var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.nateswart.com/" : "http://stats.nateswart.com/");
-  document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-  </script><script type="text/javascript">
-  try {
-  var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
-  piwikTracker.trackPageView();
-  piwikTracker.enableLinkTracking();
-  } catch( err ) {}
-  </script><noscript><p><img src="http://stats.nateswart.com/piwik.php?idsite=3" style="border:0" alt="" /></p></noscript>
-  <!-- End Piwik Tracking Code -->
 
   </body>
 </html>
